1d339de0ae67ae3d71cbbc0238c831fb42b6b442
CauaKnG <cauadefreitas030204@gmail.com>
2022-06-17 12:56:20 -0300

    Adicionando cards

--- src/Pages/Inicio/Inicio.js
+++ src/Pages/Inicio/Inicio.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BarraCategoria } from "../../Components/BarraCategoria/BarraCategoria";
 import { Carrossel } from "../../Components/Carrossel/Carrossel";
+import { Destaques } from "../../Components/Destaques/Destaques";
+
 
 export const Inicio = () => {
-    return (
-        <>
-            <BarraCategoria/>
-            <Carrossel/>
-        </>
-    )
-}
+  return (
+    <>
+      <BarraCategoria />
+      <Carrossel />
+      <Destaques />
+    </>
+  );
+};
