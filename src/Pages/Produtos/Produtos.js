0f5a75fbe82c8edb8bfde135dc3cc1abdfefd369
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-06-17 20:50:57 -0300

    Alterando database e fazendo requisição de produto

--- src/Pages/Produtos/Produtos.js
+++ src/Pages/Produtos/Produtos.js
@@ -1,13 +1,37 @@
 import { useParams } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { api } from "../../Services/api";
 
 export const Produtos = () => {
+    let lista = []
+    let { categoria } = useParams()
+    const [listaProdutos, setListaProdutos] = useState();
+    useEffect(() => {
+        const getProdutosByCategoria = async () => {
+            const response = await api.get(`/categoria/produto/dto/nome/${categoria}`)
+            console.log(response.data);
+            setListaProdutos(response.data.produtoList);
+        }
+        getProdutosByCategoria();
+    }, [])
+    //     listaProdutos.map(produto =>
+    //     console.log(produto)
+    // )
+    console.log(listaProdutos)
 
-    let {categoria} = useParams()
-    
-    return(
+    return (
         <>
-    <h1>{categoria}</h1>
+            {listaProdutos !== undefined && (
+                <div>
+                    {
+                        listaProdutos.map((produto) =>
+                            <h1 key={produto.idProduto}>{produto.idProduto}</h1>
+                        )
+                    }
+                </div>
+            )}
+
+
         </>
     )
 }
